1c76402ace9b1f31af9bdffaa7026df413edd287
Person <localboxcrox@gmail.com>
2025-08-07 13:18:04 -0400

    stuff

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 
 export default {
     async fetch(req, env) {
-        return new Response(
-            env.PUB.fetch(`${req.url}/index.html`),
-            {headers: {"content-type": "text/html"}, status: 200}
-        );
+        return env.ASSETS.fetch(req);
     }
 };
