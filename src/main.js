58af03ba86835d24ec846a3563ecd8fd27d2687e
Person <localboxcrox@gmail.com>
2025-08-16 22:09:44 -0400

    stuff

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 export default {
     async fetch() {
       return new Response(__HTML__, {
-        headers: { "content-type": "text/html; charset=utf-8" },
+        headers: {
+            "content-type": "text/html; charset=utf-8",
+            "lmao": "lol even"
+        },
         status: 200
       });
     },
